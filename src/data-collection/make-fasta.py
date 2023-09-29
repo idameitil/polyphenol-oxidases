7989c04aa9e825cd3fb70c741f99c67afeff1e99
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2023-09-29 11:41:20 +0200

    moved slides and minutes out of repo

--- src/data-collection/make-fasta.py
+++ src/data-collection/make-fasta.py
@@ -6,3 +6,9 @@ output_filename = 'data/seeds.fa'
 with open(output_filename, 'w') as outfile:
     for index, row in df.iterrows():
         outfile.write(f">{row.id}\n{row.sequence}\n")
+
+output_filename = 'data/seeds-caio.fa'
+with open(output_filename, 'w') as outfile:
+    for index, row in df.iterrows():
+        if row.studied_by_caio == 'Yes':
+            outfile.write(f">{row.id}\n{row.sequence}\n")
