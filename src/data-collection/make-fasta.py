a8e9c6d0448641c30f3c24ce185182353592264e
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2023-10-05 13:48:41 +0200

    make fasta

--- src/data-collection/make-fasta.py
+++ src/data-collection/make-fasta.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv('data/seeds.tsv', sep='\t')
+df = pd.read_csv('data/seeds.tsv', sep='\t', encoding='latin-1')
 
 output_filename = 'data/seeds.fa'
 with open(output_filename, 'w') as outfile:
     for index, row in df.iterrows():
         outfile.write(f">{row.id}\n{row.sequence}\n")
 
-output_filename = 'data/seeds-caio.fa'
-with open(output_filename, 'w') as outfile:
-    for index, row in df.iterrows():
-        if row.studied_by_caio == 'Yes':
-            outfile.write(f">{row.id}\n{row.sequence}\n")
+# output_filename = 'data/seeds-caio.fa'
+# with open(output_filename, 'w') as outfile:
+#     for index, row in df.iterrows():
+#         if row.studied_by_caio == 'Yes':
+#             outfile.write(f">{row.id}\n{row.sequence}\n")
