4ea43e8962330ced2381187b05ebf058d9056d92
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2023-10-13 15:00:04 +0200

    filtering of blast hits

---
+++ src/data-collection/filter-blast-hits/filter-blast-hits.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from Bio import SeqIO
+
+unique_hits_filename = 'data/blast/unique-hits.tsv'
+
+df = pd.read_csv(unique_hits_filename, sep='\t', names=['accession', 'score', 'evalue'])
+
+df_evaluefilter = df[df.evalue < 1e-15]
+
+output_filename = 'data/blast/unique-hits-1e-15.tsv'
+
+with open('data/blast/unique-hits-1e-15.fasta', 'w') as outfile:
+    with open('data/blast/unique-hits-1e-15-length150-1000.fasta', 'w') as outfile2:
+        fasta_sequences = SeqIO.parse('data/blast/unique-hits.fasta', 'fasta')
+        for fasta in fasta_sequences:
+            id, seq = fasta.id, str(fasta.seq)
+            if id in list(df_evaluefilter.accession):
+                outfile.write(f">{id}\n{seq}\n")
+                if len(seq) > 150 and len(seq) < 1000:
+                    outfile2.write(f">{id}\n{seq}\n")
