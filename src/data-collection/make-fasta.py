df2235e3df62d041b0a69275990e74edd82e389c
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2023-10-24 16:28:56 +0200

    seed fasta

--- src/data-collection/make-fasta.py
+++ src/data-collection/make-fasta.py
@@ -6,3 +6,11 @@ output_filename = 'data/seeds.fa'
 with open(output_filename, 'w') as outfile:
     for index, row in df.iterrows():
         outfile.write(f">{row.protein_accession}\n{row.sequence}\n")
+
+df = pd.read_excel('data/Aguilera-data/aguilera_with_seq.xlsx')
+output_filename = 'data/Aguilera-data/aguilera.fa'
+with open(output_filename, 'w') as outfile:
+    for index, row in df.iterrows():
+        if pd.isnull(row.seq):
+            continue
+        outfile.write(f">{row.protein_accession}\n{row.seq}\n")
