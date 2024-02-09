69a73cd54852356e698c3d400c6741aa9bb18e59
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-02-09 15:25:35 +0100

    made fasta with descriptive names as id

--- src/data-collection/make-fasta.py
+++ src/data-collection/make-fasta.py
@@ -7,6 +7,11 @@ with open(output_filename, 'w') as outfile:
     for index, row in df.iterrows():
         outfile.write(f">{row.protein_accession}\n{row.sequence}\n")
 
+output_filename = 'data/seeds-names.fa'
+with open(output_filename, 'w') as outfile:
+    for index, row in df.iterrows():
+        outfile.write(f">{row.descriptive_name}\n{row.sequence}\n")
+
 df = pd.read_excel('data/Aguilera-data/aguilera_with_seq.xlsx')
 output_filename = 'data/Aguilera-data/aguilera.fa'
 with open(output_filename, 'w') as outfile:
