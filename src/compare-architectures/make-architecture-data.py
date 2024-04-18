39afcdb1d12d59b0116761af16a98baf8d38b83b
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-04-18 11:46:00 +0200

    make architecture

--- src/compare-architectures/make-architecture-data.py
+++ src/compare-architectures/make-architecture-data.py
@@ -6,6 +6,8 @@ import numpy as np
 from scipy import stats
 
 threshold = 0.95
+aminoacids = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
+        'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', '-', 'X', 'J']
 
 def get_name(entry):
     return f"{entry['acc']} {entry['family']}"
@@ -15,9 +17,6 @@ def read_MSA_file(MSA_filename):
         proteins = SeqIO.parse(MSA_file, 'fasta')
         fasta_dict = {protein.id: protein.seq for protein in proteins}
     return fasta_dict
-
-aminoacids = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
-        'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', '-', 'X', 'J']
 
 def AA_to_number(AA):
     try:
@@ -66,38 +65,23 @@ def get_specific_positions_conserved_residues(accession, conserved_residues, fas
     return positions
 
 entries = [
-    {'acc': '4j3p', 'family': 'i_short_fungal', 'descriptive_name': 'AoCO4', 'domain_start': 93, 'domain_end': 322},
-    {'acc': '5m8l', 'family': 'e_chordata', 'descriptive_name': 'TyrHs', 'domain_start': 184, 'domain_end': 414},
-    {'acc': '1wx2', 'family': 'd_bacteria', 'descriptive_name': 'TyrSc', 'domain_start': 29, 'domain_end': 226}, 
-    {'acc': '2y9x', 'family': 'c_long_fungal', 'descriptive_name': 'AbPPO3', 'domain_start': 52, 'domain_end': 308}, 
-    {'acc': '2p3x', 'family': 'a_plants', 'descriptive_name': 'PPOVv', 'domain_start': 77, 'domain_end': 284} 
+    {'acc': '4j3p', 'family': 'i_short_fungal', 'descriptive_name': 'AoCO4', 'domain_start': 93, 'domain_end': 322, 'threshold':0.9},
+    {'acc': '5m8l', 'family': 'e_chordata', 'descriptive_name': 'TyrHs', 'domain_start': 159, 'domain_end': 389, 'threshold':0.95},
+    {'acc': '1wx2', 'family': 'd_bacteria', 'descriptive_name': 'TyrSc', 'domain_start': 29, 'domain_end': 226, 'threshold':0.95}, 
+    {'acc': '2y9x', 'family': 'c_long_fungal', 'descriptive_name': 'AbPPO3', 'domain_start': 52, 'domain_end': 308, 'threshold':0.95}, 
+    {'acc': '2p3x', 'family': 'a_plants', 'descriptive_name': 'PPOVv', 'domain_start': 77, 'domain_end': 284, 'threshold':0.9} 
 ]
-
-# Write manual conservation file
-# filename = "data/compare-architectures/conserved_manual.json"
-# with open(filename) as infile:
-#     conserved_dict = json.load(infile)
-
-# outfilename = f"data/compare-architectures/conserved_manual.js"
-# with open(outfilename, 'w') as outfile:
-#     outfile.write("const conservedResidues = {\n")
-#     for i in range(len(entries)):
-#         for j in range(len(entries[i])):
-#             entry = entries[i][j]
-#             outfile.write(f"\t'{get_name(entry)}': {conserved_dict[entry['acc']]},\n")
-#     outfile.write('};')
 
 # Get conserved
 for i in range(len(entries)):
     entry = entries[i]
     alignment_filename = f"data/mrbayes/all/clades/alignments/{entry['family']}-linsi.fa"
     fasta_dict = read_MSA_file(alignment_filename)
-    conserved_residues = get_conserved_residues(fasta_dict, threshold=threshold, include_aliphatic=True)
+    conserved_residues = get_conserved_residues(fasta_dict, threshold=entry['threshold'], include_aliphatic=True)
     if entry['family'] == 'e_chordata':
         print(conserved_residues)
     positions = get_specific_positions_conserved_residues(entry['descriptive_name'], conserved_residues, fasta_dict)
-    # entries[i]['conserved_positions'] = {position['pos'] + entry['domain_start']: position['AA'] for position in positions}
-    entries[i]['conserved_positions'] = {position['pos']: position['AA'] for position in positions}
+    entries[i]['conserved_positions'] = {position['pos'] + entry['domain_start']: position['AA'] for position in positions}
 
 # Write conserved file
 outfilename = f"data/compare-architectures/conserved_{threshold}.js"
@@ -129,7 +113,7 @@ with open(outfilename, 'w') as outfile:
     outfile.write("const architectures = {\n")
     for i in range(len(entries)):
         entry = entries[i]
-        outfile.write(f"\t'{get_name(entry)}': '{entry['architecture_string']}'")
+        outfile.write(f"\t'{get_name(entry)}': {{\n\t\t'domain_start\': {entry['domain_start']},\n\t\t\'architecture_string\': '{entry['architecture_string']}'\n\t\t}}")
         if i < len(entries) - 1:
             outfile.write(",\n")
         else:
@@ -148,40 +132,3 @@ with open(outfilename, 'w') as outfile:
             max_length = entry['length']
     outfile.write("};\n")
     outfile.write(f"const max_length = {max_length};\n")
-
-# accessions_filename = "data/garry-figure/family-representatives.tsv"
-# df = pd.read_csv(accessions_filename, sep='\t', names=['family', 'accession'])
-# threshold = 0.95
-
-# def replacer(s, newstring, index, nofail=False):
-#     # raise an error if index is outside of the string
-#     if not nofail and index not in range(len(s)):
-#         raise ValueError("index outside given string")
-
-#     # if not erroring, but the index is still not in the correct range..
-#     if index < 0:  # add it to the beginning
-#         return newstring + s
-#     if index > len(s):  # add it to the end
-#         return s + newstring
-
-#     # insert the new string between "slices" of the original
-#     return s[:index] + newstring + s[index + 1:]
-
-# outfilename = "data/garry-figure/conserved.out"
-# with open(outfilename, 'w') as outfile:
-#     for index, row in df.iterrows():
-#         outfile.write(row.family + '\n')
-#         outfile.write(row.accession + '\n')
-#         alignment_filename = f"data/hhblits_cazy_families/msas-family-names/{row.family}.fa"
-#         fasta_dict = read_MSA_file(alignment_filename)
-#         conserved_residues = get_conserved_residues(fasta_dict, threshold=threshold)
-#         positions = get_specific_positions_conserved_residues(row.accession, conserved_residues, fasta_dict)
-
-#         for position in positions:
-#             strings[row.accession] = replacer(s = strings[row.accession], newstring = position['AA'], index = position['pos'])
-
-# with open(outfilename_retaining, 'w') as outfile:
-#     for family in retaining:
-#         acc = family2acc[family]
-#         outfile.write(f"{family}, {acc}\n")
-#         outfile.write(strings[acc] + '\n')
