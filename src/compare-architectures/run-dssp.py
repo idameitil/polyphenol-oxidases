066db023f7bbddf5740a613558efd12b7a5217c2
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-04-18 11:49:49 +0200

    dssp script

--- src/compare-architectures/run-dssp.py
+++ src/compare-architectures/run-dssp.py
@@ -4,13 +4,16 @@ import os, sys
 
 p = PDBParser()
 
-def get_SS_string(dssp_object):
+def get_SS_string(dssp_object, discountinous):
     SS_string = 'type,end\n'
     previous = ''
+    position = 0
     for residue in dssp_object:
+        position += 1
         SS_translation = {'H': 'h', 'B': 'l', 'E': 's', 'G': 'h', 'I': 'h', 'T': 'l', 'S': 'l', '-': 'l'}
         simplified_secondary_structure = SS_translation[residue[2]]
-        position = residue[0]
+        if position in discountinous:
+            position += (discountinous[position] - position + 1)
         if simplified_secondary_structure == previous:
             continue
         if previous == 'h':
@@ -26,10 +29,16 @@ def write_ss_string(id, model_filename, outfilename):
     structure = p.get_structure(id, model_filename)
     model = structure[0]
     dssp = DSSP(model, model_filename)
-    string = get_SS_string(dssp)
+    string = get_SS_string(dssp, wanted[id])
     with open(outfilename, 'w') as outfile:
         outfile.write(string)
 
-wanted = ['4j3p', '5m8l', '1wx2', '2y9x', '2p3x']
+# Values are discontinous intervals
+wanted = {'4j3p': {1:4, 38:45}, 
+          '5m8l': {1:1}, 
+          '1wx2': {}, 
+          '2y9x': {}, 
+          '2p3x': {}}
 for id in wanted:
+    print(id)
     write_ss_string(id, f"data/compare-architectures/pdbs/{id}.pdb", f"data/compare-architectures/architecture-tables-dssp/{id}.csv")
