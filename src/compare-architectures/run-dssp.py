ac643074e3bf872f09d96a93e3b3d3a0ac39c681
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-04-23 14:12:38 +0200

    changed procedure for discontinuous

--- src/compare-architectures/run-dssp.py
+++ src/compare-architectures/run-dssp.py
@@ -9,19 +9,25 @@ def get_SS_string(dssp_object, discountinous):
     previous = ''
     position = 0
     for residue in dssp_object:
+        print(residue)
+        previous_position = position
         position += 1
         SS_translation = {'H': 'h', 'B': 'l', 'E': 's', 'G': 'h', 'I': 'h', 'T': 'l', 'S': 'l', '-': 'l'}
-        simplified_secondary_structure = SS_translation[residue[2]]
         if position in discountinous:
-            position += (discountinous[position] - position + 1)
+            simplified_secondary_structure = 'u'
+            position = discountinous[position] + 1
+        else:
+            simplified_secondary_structure = SS_translation[residue[2]]
         if simplified_secondary_structure == previous:
             continue
         if previous == 'h':
-            SS_string += f"h,{position-1}\n"
+            SS_string += f"h,{previous_position}\n"
         elif previous == 's':
-            SS_string += f"s,{position-1}\n"
+            SS_string += f"s,{previous_position}\n"
         elif previous == 'l':
-            SS_string += f"l,{position-1}\n"
+            SS_string += f"l,{previous_position}\n"
+        elif previous == 'u':
+            SS_string += f"u,{previous_position}\n"
         previous = simplified_secondary_structure
     return SS_string
 
@@ -35,9 +41,9 @@ def write_ss_string(id, model_filename, outfilename):
 
 # Values are discontinous intervals
 wanted = {'4j3p': {1:4, 38:45}, 
-          '5m8l': {1:25}, 
-          '1wx2': {}, 
-          '2y9x': {}, 
+          '5m8l': {1:24}, 
+          '1wx2': {1:1}, 
+          '2y9x': {1:1}, 
           '2p3x': {},
           'F4PFF7': {},
           'V3ZAB2': {},
