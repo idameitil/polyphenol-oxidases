4fc729cee860af396871922219f60b226b552106
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-06-13 15:54:47 +0200

    aligned architecture figure

---
+++ data/compare-architectures/not-aligned/name2color.js
@@ -0,0 +1,10 @@
+const name2color = {
+	'a (plant), 2p3x': [16, 174, 253], 
+	'c (long fungal), 2y9x': [247, 18, 82],
+	'd (bacteria), 1wx2': [17, 157, 88],
+	'e (chordata), 5m8l': [105, 133, 181],
+	'f (mollusc), V3ZAB2': [236, 215, 90],
+	'h (oomycota), D0N318': [82, 9, 155],
+	'i (short fungal), 4j3p': [251, 139, 52],
+	'j (zoopagomycota), F4PFF7': [240, 86, 234]
+};
