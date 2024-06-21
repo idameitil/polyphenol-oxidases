8817850a2a7227227a1a6b1aed9a542060a2d845
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-06-21 15:00:44 +0200

    fungal one per order

--- src/proteome-tree/get-aligned-selected-sequences.py
+++ src/proteome-tree/get-aligned-selected-sequences.py
@@ -1,35 +1,104 @@
 from Bio import SeqIO
 import pandas as pd
 
-def get_fasta_sequences(fasta_filename = 'data/pfam/PF00264.alignment.uniprot.fa'):
-    fasta_alignment = SeqIO.parse(fasta_filename, 'fasta')
+
+def get_fasta_sequences_location(fasta_filename):
+    fasta_alignment = SeqIO.parse(fasta_filename, "fasta")
     acc2seq = {}
     for fasta in fasta_alignment:
-        acc = fasta.id.split('.')[0]
-        version = fasta.id.split('.')[1][0]
-        position = fasta.id.split('/')[1]
-        trimmed_seq = fasta.seq.replace('-', '')
-        acc2seq[f"{acc},{position}"] = {'acc': acc, 
-                                        'version': version, 
-                                        'position': position, 
-                                        'trimmed_seq': trimmed_seq}
+        acc = fasta.id.split(".")[0]
+        version = fasta.id.split(".")[1][0]
+        position = fasta.id.split("/")[1]
+        trimmed_seq = fasta.seq.replace("-", "")
+        acc2seq[f"{acc},{position}"] = {
+            "acc": acc,
+            "version": version,
+            "position": position,
+            "trimmed_seq": trimmed_seq,
+        }
     return acc2seq
 
-acc2seq_trimmed = get_fasta_sequences()
 
-def get_selected_ids():
-    infile_name = f'data/proteome-tree/selected-sequences-all-class.txt'
+def get_fasta_sequences_seeds(fasta_filename):
+    fasta_alignment = SeqIO.parse(fasta_filename, "fasta")
+    acc2seq = {}
+    for fasta in fasta_alignment:
+        acc2seq[fasta.id] = fasta.seq
+    return acc2seq
+
+
+acc2seq_trimmed = get_fasta_sequences_location("data/pfam/PF00264.alignment.uniprot.fa")
+acc2seq_seeds_trimmed = get_fasta_sequences_seeds("data/seeds-trimmed.fa")
+seeds_include = [
+    "ScTyr",
+    "BmTyr",
+    "VvPPO",
+    "IbCO",
+    "EdHc",
+    "RvHc",
+    "CgPPO-473",
+    "CgPPO-266",
+    "MtPPO-809",
+    "MtPPO-010",
+    "PpPPO-c2092",
+    "MtPPO7",
+    "AbTyr",
+    "NpsF",
+    "SlPPO1",
+    "AoMelO",
+    "HjTyr",
+    "PsTyr",
+]
+
+
+def get_selected_ids(domain, rank):
+    infile_name = f"data/proteome-tree/selected-sequences-{domain}-{rank}.txt"
     with open(infile_name) as infile:
         selected = [line.strip() for line in infile]
     return selected
 
-def write_trimmed_fasta(domain, rank):
-    """for mafft tree"""
-    selected = get_selected_ids()
-    output_filename = f"data/proteome-tree/{domain}-one_proteome_per_{rank}.trimmed.fa"
-    with open(output_filename, 'w') as outfile:
-        for entry in acc2seq_trimmed:
-            if entry in selected:
-                outfile.write(f">{acc2seq_trimmed[entry]['acc']}.{acc2seq_trimmed[entry]['version']}/{acc2seq_trimmed[entry]['position']}\n{acc2seq_trimmed[entry]['trimmed_seq']}\n")
 
-write_trimmed_fasta('all', 'class')
+def get_filtered_out_ids():
+    infile_name = f"data/proteome-tree/filtered-out-sequences-all-class.txt"
+    with open(infile_name) as infile:
+        filtered_out = [line.strip() for line in infile]
+    return filtered_out
+
+
+def write_trimmed_fasta(domain, rank):
+    selected = get_selected_ids(domain, rank)
+    output_filename = f"data/proteome-tree/{domain}-one_proteome_per_{rank}.trimmed.fa"
+    with open(output_filename, "w") as outfile:
+        if domain == 'all':
+            for entry in acc2seq_trimmed:
+                if entry in selected:
+                    outfile.write(
+                        f">{acc2seq_trimmed[entry]['acc']}.{acc2seq_trimmed[entry]['version']}/{acc2seq_trimmed[entry]['position']}\n{acc2seq_trimmed[entry]['trimmed_seq']}\n"
+                    )
+            for acc in acc2seq_seeds_trimmed:
+                if acc in seeds_include:
+                    outfile.write(f">{acc}\n{acc2seq_seeds_trimmed[acc]}\n")
+        if domain == 'fungi':
+            all_selected = get_selected_ids(domain='all', rank='class')
+            for entry in acc2seq_trimmed:
+                # check if it's in the selection of all
+                id_formatted = f"{acc2seq_trimmed[entry]['acc']},{acc2seq_trimmed[entry]['position']}"
+                if id_formatted in selected and id_formatted not in all_selected:
+                    outfile.write(
+                        f">{acc2seq_trimmed[entry]['acc']}.{acc2seq_trimmed[entry]['version']}/{acc2seq_trimmed[entry]['position']}\n{acc2seq_trimmed[entry]['trimmed_seq']}\n"
+                    ) 
+
+
+def write_filtered_out():
+    filtered_out_ids = get_filtered_out_ids()
+    output_filename = f"data/proteome-tree/filtered-out-all-one_proteome_per_class.trimmed.fa"
+    with open(output_filename, "w") as outfile:
+        for entry in acc2seq_trimmed:
+            if entry in filtered_out_ids:
+                outfile.write(
+                    f">{acc2seq_trimmed[entry]['acc']}.{acc2seq_trimmed[entry]['version']}/{acc2seq_trimmed[entry]['position']}\n{acc2seq_trimmed[entry]['trimmed_seq']}\n"
+                )
+
+write_trimmed_fasta("all", "class")
+write_trimmed_fasta("fungi", "order")
+write_filtered_out()
--- src/proteome-tree/get-proteome-sequences.py
+++ src/proteome-tree/get-proteome-sequences.py
@@ -3,43 +3,47 @@ import json
 import pandas as pd
 import sys
 
+
 def make_json_dict(json_list):
     json_dict = {}
     for entry in json_list:
-        json_dict[entry['metadata']['accession']] = entry
+        json_dict[entry["metadata"]["accession"]] = entry
     return json_dict
+
 
 def make_protein2proteome_dict(json):
     protein2proteome = {}
     for entry in json:
-        protein_acc = entry['metadata']['accession']
-        if len(entry['proteome_subset']) > 1:
+        protein_acc = entry["metadata"]["accession"]
+        if len(entry["proteome_subset"]) > 1:
             print(entry)
-        proteome_acc = entry['proteome_subset'][0]['accession']
+        proteome_acc = entry["proteome_subset"][0]["accession"]
         protein2proteome[protein_acc] = proteome_acc.upper()
     return protein2proteome
 
-class proteomeData():
 
-    df = pd.read_csv('data/proteome-tree/proteome-data.tsv', sep='\t')
-    json_list = json.load(open('data/pfam/protein-matching-PF00264.json'))
+class proteomeData:
+
+    df = pd.read_csv("data/proteome-tree/proteome-data.tsv", sep="\t")
+    json_list = json.load(open("data/pfam/protein-matching-PF00264.json"))
     json_dict = make_json_dict(json_list)
-    export_json = json.load(open('data/proteome-tree/export.json'))
+    export_json = json.load(open("data/proteome-tree/export.json"))
     protein2proteome = make_protein2proteome_dict(export_json)
 
-    def __init__(self, domain, rank):
+    def __init__(self, domain, rank, score_threshold):
         self.domain = domain
         self.rank = rank
+        self.score_threshold = score_threshold
         self.selected_proteomes2species = self.get_selected_proteomes()
         self.accs_from_selected_proteomes = self.get_accs_from_selected_proteomes()
-        self.filtered_sequences = self.filter_sequences()
+        self.filtered_sequences, self.filtered_out_sequences = self.filter_sequences(self.score_threshold)
 
-    def get_selected_proteomes(self): 
-        if self.domain == 'all' and self.rank == 'class':
-            df = pd.read_excel('data/proteome-tree/class-representatives.xlsx')
-        elif self.domain == 'fungi' and self.rank == 'order':
-            df = pd.read_excel('data/proteome-tree/fungal-order-representatives.xlsx')
-        selected_proteomes2species = {} 
+    def get_selected_proteomes(self):
+        if self.domain == "all" and self.rank == "class":
+            df = pd.read_excel("data/proteome-tree/class-representatives.xlsx")
+        elif self.domain == "fungi" and self.rank == "order":
+            df = pd.read_excel("data/proteome-tree/fungal-order-representatives.xlsx")
+        selected_proteomes2species = {}
         for index, row in df.iterrows():
             selected_proteomes2species[row.genome_id] = row.species
         return selected_proteomes2species
@@ -47,34 +51,59 @@ class proteomeData():
     def get_accs_from_selected_proteomes(self):
         accs_from_selected_proteomes = list()
         for protein_acc in self.protein2proteome:
+            if self.protein2proteome[protein_acc] == "UP000515154":
+                print(protein_acc)
             if self.protein2proteome[protein_acc] in self.selected_proteomes2species:
                 accs_from_selected_proteomes.append(protein_acc)
         return accs_from_selected_proteomes
-    
-    def filter_sequences(self):
-        fasta_sequences = SeqIO.parse('data/pfam/protein-matching-PF00264-shortheaders.fasta', 'fasta')
+
+    def filter_sequences(self, score_threshold):
+        fasta_sequences = SeqIO.parse(
+            "data/pfam/protein-matching-PF00264-shortheaders.fasta", "fasta"
+        )
         filtered_sequences = []
+        filtered_out_sequences = []
         count = 0
         for fasta in fasta_sequences:
             if fasta.id not in self.accs_from_selected_proteomes:
                 continue
             count += 1
-            for entry_location in self.json_dict[fasta.id]['entries'][0]['entry_protein_locations']:
-                score = entry_location['score']
-                start = entry_location['fragments'][0]['start']
-                end = entry_location['fragments'][0]['end']
-                if score > 1e-20:
-                    continue
-                filtered_sequences.append({'acc': fasta.id, 'position': f"{start}-{end}"})
+            for entry_location in self.json_dict[fasta.id]["entries"][0]["entry_protein_locations"]:
+                score = entry_location["score"]
+                start = entry_location["fragments"][0]["start"]
+                end = entry_location["fragments"][0]["end"]
+                if score > score_threshold:
+                    filtered_out_sequences.append(
+                    {"acc": fasta.id, "position": f"{start}-{end}"}
+                )
+                else:
+                    filtered_sequences.append(
+                        {"acc": fasta.id, "position": f"{start}-{end}"}
+                    )
         print(f"Total: {count}")
         print(f"Succeeded: {len(filtered_sequences)}")
-        return filtered_sequences
-    
+        return filtered_sequences, filtered_out_sequences
+
     def write_selected_sequences(self):
-        filename = f'data/proteome-tree/selected-sequences-{self.domain}-{self.rank}.txt'
-        with open(filename, 'w') as outfile:
+        filename = (
+            f"data/proteome-tree/selected-sequences-{self.domain}-{self.rank}.txt"
+        )
+        with open(filename, "w") as outfile:
             for entry in self.filtered_sequences:
                 outfile.write(f"{entry['acc']},{entry['position']}\n")
 
-all_class = proteomeData(domain='all', rank='class')
+    def write_filtered_out_sequences(self):
+        filename = (
+            f"data/proteome-tree/filtered-out-sequences-{self.domain}-{self.rank}.txt"
+        )
+        with open(filename, "w") as outfile:
+            for entry in self.filtered_out_sequences:
+                outfile.write(f"{entry['acc']},{entry['position']}\n")
+
+
+all_class = proteomeData(domain="all", rank="class", score_threshold=1e-20)
 all_class.write_selected_sequences()
+all_class.write_filtered_out_sequences()
+
+all_fungal = proteomeData(domain="fungi", rank="order", score_threshold=1)
+all_fungal.write_selected_sequences()
