f2d640dd1beb635e6bb6302e1b883a888e491d83
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-06-21 14:59:32 +0200

    analyse run script

--- src/proteome-tree/analyse-run.R
+++ src/proteome-tree/analyse-run.R
@@ -1,29 +1,46 @@
 library(ggplot2)
 library(ggpubr)
 
-par(mfrow=c(2,1))
-
 setwd("/Users/idamei/polyphenol-oxidases/data/mrbayes/all/run")
 df = read.table(file = 'out.nex.mcmc', sep = '\t', header = TRUE, skip=5)
 df['GenMio'] = df['Gen'] / 1000000
 all <- ggplot(df, aes(x=GenMio, y=AvgStdDev.s.)) +
   geom_point() +
-  ylim(0,0.2)
+  ylim(0,0.2) +
+  xlim(0, 60)
 
 setwd("/Users/idamei/hpc/fungal-10-mixed")
 df = read.table(file = 'test.nex.mcmc', sep = '\t', header = TRUE, skip=5)
 df['GenMio'] = df['Gen'] / 1000000
 fungal <- ggplot(df, aes(x=GenMio, y=AvgStdDev.s.)) +
   geom_point() +
-  ylim(0,0.2)
+  ylim(0,0.2) +
+  xlim(0, 60)
 
-setwd("/Users/idamei/hpc/fungal-new")
+setwd("/Users/idamei/hpc/fungal-newnew")
 df = read.table(file = 'test.nex.mcmc', sep = '\t', header = TRUE, skip=5)
 df['GenMio'] = df['Gen'] / 1000000
 fungal_new <- ggplot(df, aes(x=GenMio, y=AvgStdDev.s.)) +
   geom_point() +
-  ylim(0,0.2)
+  ylim(0,0.2) +
+  xlim(0, 75)
 
-ggarrange(all, fungal, fungal_new,
+setwd("/Users/idamei/hpc/all-new")
+df = read.table(file = 'out.nex.mcmc', sep = '\t', header = TRUE, skip=5)
+df['GenMio'] = df['Gen'] / 1000000
+all_new <- ggplot(df, aes(x=GenMio, y=AvgStdDev.s.)) +
+  geom_point() +
+  ylim(0,0.2) +
+  xlim(0, 70)
+
+setwd("/Users/idamei/hpc/all-seeds-0619")
+df = read.table(file = 'out.nex.mcmc', sep = '\t', header = TRUE, skip=5)
+df['GenMio'] = df['Gen'] / 1000000
+all_seeds <- ggplot(df, aes(x=GenMio, y=AvgStdDev.s.)) +
+  geom_point() +
+  ylim(0,0.2) +
+  xlim(0, 70)
+
+ggarrange(all, all_new, all_seeds
           labels = c("A", "B", "C"),
           ncol = 3, nrow = 1)
