0b071230307850d9f6c05486b9cfc3345722d66d
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-07-05 10:30:01 +0200

    used full sequences for seeds fasta

--- src/data-collection/make-fasta.py
+++ src/data-collection/make-fasta.py
@@ -5,12 +5,12 @@ df = pd.read_csv('data/seeds.tsv', sep='\t', encoding='latin-1')
 output_filename = 'data/seeds.fa'
 with open(output_filename, 'w') as outfile:
     for index, row in df.iterrows():
-        outfile.write(f">{row.descriptive_name}\n{row.sequence}\n")
+        outfile.write(f">{row.descriptive_name}\n{row.full_sequence}\n")
 
-df = pd.read_excel('data/Aguilera-data/aguilera_with_seq.xlsx')
-output_filename = 'data/Aguilera-data/aguilera.fa'
-with open(output_filename, 'w') as outfile:
-    for index, row in df.iterrows():
-        if pd.isnull(row.seq):
-            continue
-        outfile.write(f">{row.protein_accession}\n{row.seq}\n")
+# df = pd.read_excel('data/Aguilera-data/aguilera_with_seq.xlsx')
+# output_filename = 'data/Aguilera-data/aguilera.fa'
+# with open(output_filename, 'w') as outfile:
+#     for index, row in df.iterrows():
+#         if pd.isnull(row.seq):
+#             continue
+#         outfile.write(f">{row.protein_accession}\n{row.seq}\n")
