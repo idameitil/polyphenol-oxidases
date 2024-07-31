4cd24bb66593ae4b75f22100e0ee2326456ab123
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-07-31 11:25:32 +0200

    Included things without class in boxplots and made grey

--- src/boxplots/make-boxplots.R
+++ src/boxplots/make-boxplots.R
@@ -4,8 +4,16 @@ library(readr)
 
 setwd("/Users/idamei/polyphenol-oxidases")
 
-data = read.table('data/boxplots/boxplot-data.tsv', sep = '\t', header=TRUE, fill = TRUE)
 data <- read_delim("data/boxplots/boxplot-data.tsv", na = c("", "NA"), delim='\t')
+
+na_df = data[is.na(data$class),]
+
+data$class[data$order == 'Mycoplasmoidales'] <- 'Mycoplasmoidales'
+data$class[data$order == 'Polyangiales'] <- 'Polyangiales'
+data$class[data$order == 'Peronosporales'] <- 'Peronosporales'
+data$class[data$order == 'Saprolegniales'] <- 'Saprolegniales'
+data$class[data$order == 'Polyangiales'] <- 'Polyangiales'
+data$class[data$order == 'Testudines'] <- 'Testudines'
 
 unique_values_in_order <- unique(data$class[match(data$class, data$class)])
 data$class <- factor(data$class, levels = unique_values_in_order)
@@ -22,7 +30,6 @@ filtered_classes <- class_counts %>%
   filter(count >= 5) %>%
   select('class')
 
-# Filter the original data to keep only classes with 5 or more members
 filtered_data <- data %>%
   filter(class %in% filtered_classes$class & !is.na(class) & class != '')
 
@@ -44,14 +51,14 @@ dev.off()
 
 # flipped
 p1 <- ggplot(filtered_data, aes(x = class, y = count_tyrosinases)) +
-  geom_boxplot() +
+  geom_boxplot(color = 'grey50') +
   geom_jitter(size = 0.1, width = 0.3, height = 0, alpha = 0.7, color = "blue") +
   xlab('Taxonomic class') +
   ylab('Number of PPO genes') +
   theme(axis.text.x = element_text()) +
   scale_y_continuous(breaks = y_breaks) +
   coord_flip()
-
+p1
 pdf('manuscript/figures/boxplot_flipped.pdf', width = 11, height = 15)
 p1
 dev.off()
