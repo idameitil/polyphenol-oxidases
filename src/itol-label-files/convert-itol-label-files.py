1ed2db0432c991ae2830d51f16f72a57529729df
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-08-16 13:30:35 +0200

    converted itol label files to new format

--- src/itol-label-files/convert-itol-label-files.py
+++ src/itol-label-files/convert-itol-label-files.py
@@ -1,24 +1,25 @@
 from Bio import SeqIO
 
-fasta = 'data/proteome-tree/all-one_proteome_per_class.trimmed.fa'
-fasta_filtered_out = 'data/epa-ng/filtered-out/query.fa'
-fasta_fungi = 'data/proteome-tree/fungi-one_proteome_per_order.trimmed.fa'
-fasta_fungi2 = 'data/proteome-tree/fungal-one_proteome_per_order.trimmed.fa'
+fasta = 'data/proteome-tree/sequences-all-class-filtered.trimmed.fa'
+# fasta_filtered_out = 'data/epa-ng/filtered-out/query.fa'
+# fasta_fungi = 'data/proteome-tree/fungi-one_proteome_per_order.trimmed.fa'
+# fasta_fungi2 = 'data/proteome-tree/fungal-one_proteome_per_order.trimmed.fa'
 
 def get_ids(fastas):
     ids = {}
     for fasta in fastas:
         fasta_sequences = SeqIO.parse(fasta, 'fasta')
         for fasta_sequence in fasta_sequences:
-            acc = fasta_sequence.id.split('.')[0]
+            acc = fasta_sequence.id.split('/')[0]
             if acc in ids:
-                if fasta_sequence.id.replace('/', '0') not in ids[acc]:
-                    ids[acc].append(fasta_sequence.id.replace('/', '0'))
+                if fasta_sequence.id.replace('/', '.') not in ids[acc]:
+                    ids[acc].append(fasta_sequence.id.replace('/', '.'))
             else:
-                ids[acc] = [fasta_sequence.id.replace('/', '0')]
+                ids[acc] = [fasta_sequence.id.replace('/', '.')]
     return ids
 
-ids = get_ids([fasta, fasta_filtered_out, fasta_fungi, fasta_fungi2])
+# ids = get_ids([fasta, fasta_filtered_out, fasta_fungi, fasta_fungi2])
+ids = get_ids([fasta])
 
 def translate(infile_name, outfile_name, sep):
     with open(infile_name) as infile, open(outfile_name, 'w') as outfile:
