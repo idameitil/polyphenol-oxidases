281a54411ce24c07171d84380d8528b80223b6ca
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-08-26 13:19:29 +0200

    short fungal tree, don't add seeds after

--- src/short-fungal-tree/get-sequences.py
+++ src/short-fungal-tree/get-sequences.py
@@ -9,17 +9,8 @@ with open(acc_file) as file:
 fasta = 'data/epa-ng/fungi-order/ref-query.fa'
 fasta_sequences = SeqIO.parse(fasta, 'fasta')
 
-# read seed fasta
-seed_fasta = 'data/seeds-trimmed.fa'
-seed_sequences = SeqIO.parse(seed_fasta, 'fasta')
-
-seeds_include = ['AoCO4', 'CgPPO-473', 'CgPPO-266', 'MtPPO-809', 'MtPPO-010', 'PpPPO-c2092', 'MtPPO7']
-
 # write
 with open('data/short-fungal-tree/short-fungal.fasta', 'w') as outfile:
     for fasta in fasta_sequences:
         if fasta.id in accs:
-            outfile.write(f">{fasta.id}\n{fasta.seq}\n")
-    for fasta in seed_sequences:
-        if fasta.id in seeds_include:
             outfile.write(f">{fasta.id}\n{fasta.seq}\n")
