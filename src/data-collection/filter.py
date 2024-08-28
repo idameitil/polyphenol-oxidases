e81761ef94d411957a4bc4c1f75f6cf963d9f7e7
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-08-28 15:30:24 +0200

    filter for cd-hit reduced tree

--- src/data-collection/filter.py
+++ src/data-collection/filter.py
@@ -1,29 +1,33 @@
 from Bio import SeqIO
 import json
 
-fasta_sequences = SeqIO.parse('data/pfam/protein-matching-PF00264-shortheaders.fasta', 'fasta')
+fasta_sequences = SeqIO.parse('data/pfam/protein-matching-PF00264.fasta', 'fasta')
 
-output_filename = 'data/pfam/protein-matching-PF00264-shortheaders-filtered25.fasta'
+output_filename = 'data/pfam/protein-matching-PF00264-filtered20.fasta'
 
 json_list = json.load(open('data/pfam/protein-matching-PF00264.json'))
+
+threshold = 1e-25
 
 json_dict = {}
 for entry in json_list:
     json_dict[entry['metadata']['accession']] = entry
-
+count = 0
 with open(output_filename, 'w') as outfile:
     for fasta in fasta_sequences:
-        if len(json_dict[fasta.id]['entries']) > 1:
-            print(f"{fasta.id} has several entries")
+        accession = fasta.id.split('|')[0]
+        if len(json_dict[accession]['entries']) > 1:
+            print(f"{accession} has several entries")
             continue
-        if len(json_dict[fasta.id]['entries'][0]['entry_protein_locations']) > 1:
-            print(f"{fasta.id} has several entry_protein_locations")
-            continue
-        if len(json_dict[fasta.id]['entries'][0]['entry_protein_locations'][0]['fragments']) > 1:
-            print(f"{fasta.id} has several fragments")
-            continue
-        hit_length = json_dict[fasta.id]['entries'][0]['entry_protein_locations'][0]['fragments'][0]['end'] - json_dict[fasta.id]['entries'][0]['entry_protein_locations'][0]['fragments'][0]['start']
-        score = json_dict[fasta.id]['entries'][0]['entry_protein_locations'][0]['score']
-        # if len(fasta.seq) > 100 and len(fasta.seq) < 1000 and score < 1e-5 and hit_length > 100:
-        if len(fasta.seq) > 100 and len(fasta.seq) < 1000 and score < 1e-25 and hit_length > 100:
-            outfile.write(f">{fasta.id}\n{fasta.seq}\n")
+        for entry in json_dict[accession]['entries'][0]['entry_protein_locations']:
+            if len(entry['fragments']) > 1:
+                print(f"{accession} has several fragments")
+                continue
+            score = entry['score']
+            start = entry['fragments'][0]['start']
+            end = entry['fragments'][0]['end']
+            if score < threshold:
+                outfile.write(f">{accession}/{start}-{end}\n{fasta.seq}\n")
+            else:
+                count += 1
+print(count)
